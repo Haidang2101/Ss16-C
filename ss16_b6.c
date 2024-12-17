638701830b7a9bf15735ca6ed640613ca286a201
Haidang2101 <haidanghanoi2003@gmail.com>
2024-12-17 08:08:58 +0700

    Update ss16_b6.c

--- ss16_b6.c
+++ ss16_b6.c
@@ -2,11 +2,11 @@
 void search(int *arr,int number,int length){
     for(int i=0;i<length;i++){
         if(arr[i]==number){
-            printf("vi tri cua phan tu %d la  %d : ", number , i);
+            printf("vi tri cua phan tu %d laÂ  %d : ", number , i);
         }
     }
 }
-int main(int argc, const char * argv[]) {
+int main() {
     int arr[]={1,2,3,4,5,6,7,8,9,10};
     int length =sizeof(arr)/sizeof(int);
     int *arrptr = arr;
